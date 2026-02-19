1afde9cef13d02bac46f9f5aa1bb254738414ec7
Yadgarov Fayzullo <yadgarovfayzullo0@gmail.com>
2026-02-20 01:20:51 +0500

    font changed to manrope

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
+const manrope = Manrope({
   variable: "--font-geist-sans",
   subsets: ["latin", "cyrillic"],
   display: "swap",
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <body className={`${manrope.variable} font-sans antialiased`}>
         {children}
       </body>
     </html>
